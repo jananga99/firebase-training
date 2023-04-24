253058a5814ef8f907ea2ebe8d626cd9779db44a
jananga99 <jananga1999@gmail.com>
2023-04-24 18:52:37 +0530

    iOS batteryu set

--- lib/services/battery.service.dart
+++ lib/services/battery.service.dart
@@ -12,26 +12,17 @@ Future<int> getBatteryPercentage() async {
   final platformDefault = getPlatform();
   if (platformDefault == "web") {
     return -2;
-  }
-  switch (platformDefault) {
-    case TargetPlatform.android:
-      const platform = MethodChannel("samples.flutter.dev/battery");
-      int batteryLevel;
-      try {
-        batteryLevel = await platform.invokeMethod('getBatteryLevel');
-      } on PlatformException catch (e) {
-        batteryLevel = -1;
-      }
-      return batteryLevel;
-    case TargetPlatform.iOS:
-      return -1;
-    case TargetPlatform.macOS:
-      return -1;
-    case TargetPlatform.windows:
-      return -1;
-    case TargetPlatform.linux:
-      return -1;
-    default:
-      return -1;
+  } else if (platformDefault == TargetPlatform.android ||
+      platformDefault == TargetPlatform.iOS) {
+    const platform = MethodChannel("samples.flutter.dev/battery");
+    int batteryLevel;
+    try {
+      batteryLevel = await platform.invokeMethod('getBatteryLevel');
+    } on PlatformException {
+      batteryLevel = -1;
+    }
+    return batteryLevel;
+  } else {
+    return -2;
   }
 }
