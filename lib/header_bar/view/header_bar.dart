72b6295d7dceb1c5cfca0467a16c40b4bb4c85d3
jananga99 <jananga1999@gmail.com>
2023-04-30 14:51:20 +0530

    Bloc added for getting batteyr percentage

---
+++ lib/header_bar/view/header_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project1/header_bar/widgets/home_icon.dart';
+import 'package:project1/header_bar/widgets/notification_icon.dart';
+import 'package:project1/repositories/battery_repository/battery_repository.dart';
+
+import '../bloc/battery_bloc.dart';
+import '../widgets/battery.dart';
+import '../widgets/profile.dart';
+
+class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
+  final BatteryRepository _batteryRepository;
+
+  @override
+  final Size preferredSize;
+
+  HeaderBar({super.key, BatteryRepository? batteryRepository})
+      : preferredSize = const Size.fromHeight(56.0),
+        _batteryRepository = batteryRepository ?? BatteryRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: const Color(0xff00ffff),
+      elevation: 0,
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 40),
+          child: const HomeIcon(),
+        ),
+        const NotificationIcon(),
+        Container(
+            margin: const EdgeInsets.only(right: 30), child: const Profile()),
+        RepositoryProvider(
+          create: (context) => _batteryRepository,
+          child: BlocProvider(
+            create: (context) => BatteryBloc(_batteryRepository)
+              ..add(BatteryPercentageFetchingStarted()),
+            child: const Battery(),
+          ),
+        )
+      ],
+    );
+  }
+}
